cdbd30bf77f26ae2a5b678809ce3b91c8f9a6dfb
Jahnavi Maddhuri <jahnavi.maddhuri@gmail.com>
2024-09-21 15:46:36 -0400

    remove wildcard import

--- crime_analyze.py
+++ crime_analyze.py
@@ -1,7 +1,7 @@
 '''This file reads a crime dataset based on LAPD.
 It produces summary statistics and data visualizations'''
 
-from mylib.lib import *
+from mylib.lib import print_stats,plot_hist_time_occ,geo_plot_crime_rate,hist_plot_vict_age
 
 df = read_zip()
 
