1d6133dc053540954cb523781eaee0808e633225
Jahnavi Maddhuri <jahnavi.maddhuri@gmail.com>
2024-09-21 15:44:36 -0400

    use snake case in crime analyze script

--- crime_analyze.py
+++ crime_analyze.py
@@ -1,14 +1,14 @@
 '''This file reads a crime dataset based on LAPD.
 It produces summary statistics and data visualizations'''
 
-from mylib.lib import read_zip, print_stats, plot_hist_Time_Occ, geo_plot_Crime_Rate, hist_plot_Vict_Age
+from mylib.lib import *
 
 df = read_zip()
 
 print_stats(df)
 
-plot_hist_Time_Occ(df)
+plot_hist_time_occ(df)
 
-geo_plot_Crime_Rate(df)
+geo_plot_crime_rate(df)
 
-hist_plot_Vict_Age(df)
+hist_plot_vict_age(df)
